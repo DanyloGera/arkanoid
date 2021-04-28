5521c4f2ac734b997e3a44927fffa9794ce3fced
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-04-28 11:16:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function ballLeaveScreen() {
         paddle.reset(game.world.width*0.5, game.world.height-10);
         game.input.onDown.addOnce(function(){
             lifeLostText.visible = false;
-            ball.body.velocity.set(200, -200);
+            ball.body.velocity.set(220, -220);
         }, this);
     }
     else {
@@ -131,7 +131,7 @@ function ballHitPaddle(ball, paddle) {
 }
 function startGame() {
     startButton.destroy();
-    ball.body.velocity.set(200, -200);
+    ball.body.velocity.set(220, -220);
     playing = true;
 }
 
