2444b31ecdce76523a49f02004151857b3372c3f
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-04-28 09:04:13 +0300

    brick

--- script.js
+++ script.js
@@ -69,8 +69,8 @@ function update() {
 }
 function initBricks() {
     brickInfo = {
-        width: 70,
-        height: 30,
+        width: 90,
+        height: 35,
         count: {
             row: 7,
             col: 3
